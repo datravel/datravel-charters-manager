1d13f6fed1577c4df44b0b82172384fba3dfc1ac
Sergey Popinevskiy <gurza000@gmail.com>
2016-04-23 21:52:03 +0300

    Implement function to open file from remote source

--- importer.py
+++ importer.py
@@ -2,5 +2,20 @@
 #coding: utf-8
 
 
+def open_remote(source):
+    import urllib2
+    from config import sources
+
+    # TODO: Notice. You can implement switch for proto and auth type here
+    remote_source_obj = filter(lambda source_obj: source_obj['name'] == source, sources)
+    top_level_url = remote_source_obj[0]['remote_proto'] + '://' \
+        + remote_source_obj[0]['remote_user'] + ':' + remote_source_obj[0]['remote_password'] + '@' \
+        + remote_source_obj[0]['remote_host'] \
+        + remote_source_obj[0]['path_to_file']
+
+    opener = urllib2.build_opener()
+    return opener.open(top_level_url)
+
+
 if __name__ == '__main__':
     pass
