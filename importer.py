5bc0c2ff4674e3dbf83431624d2bfc09c98d3596
Sergey Popinevskiy <gurza000@gmail.com>
2016-04-25 12:20:08 +0300

    Create skeleton of preimport handler

--- importer.py
+++ importer.py
@@ -34,21 +34,72 @@ def update_local(source):
     return local_file_fn
 
 
-def get_tickets(source):
+def get_tickets_from_local(csv_fn):
     import csv
 
-    local_source_fn = update_local(source)
-    with open(local_source_fn, "rb") as csvfile:
-        datareader=csv.reader(csvfile)
+    with open(csv_fn, "rb") as csvfile:
+        datareader=csv.reader(csvfile, delimiter='\t', lineterminator='\r\n')
+        is_first_line = True
         for row in datareader:
+            if is_first_line:
+                is_first_line = False
+                continue
             yield row
 
 
-def import_charter_tickets(source):
+def save_ticket_in_local(tkt, csv_fn):
+    pass
+
+
+def check_is_title(tkt_item):
+    return False
+
+
+def check_is_zero_price(tkt_item):
+    return False
+
+
+def convert_icao_to_iata(tkt_item):
+    from config import icao_to_iata
+
+    flag = 0
+    return tkt_item, flag
+
+
+def preimport_handler(in_fn):
+    out_fn = in_fn + '.import'
+
     count = 0
-    for row in get_tickets(source):
+    count_is_title = count_is_zero_price = 0
+    count_icao = 0
+    for tkt in get_tickets_from_local(in_fn):
+        if check_is_title(tkt):
+            count_is_title += 1
+            continue
+        if check_is_zero_price(tkt):
+            count_is_zero_price += 1
+            continue
+
+        count_icao_fl = 0
+        tkt, count_icao_fl = convert_icao_to_iata(tkt)
+        count_icao += count_icao_fl
+
+        save_ticket_in_local(tkt, out_fn)
+
         count += 1
+
+    # Debug
     print count
+    print 'Filtered (titles):', count_is_title
+    print 'Filtered (zero prices):', count_is_zero_price
+    print 'Converted tkts (icao to iata):', count_icao
+
+    return out_fn
+
+
+def import_charter_tickets(source):
+    local_source_fn = update_local(source)
+    local_source_filtered_fn = preimport_handler(local_source_fn)
 
 
 if __name__ == '__main__':
