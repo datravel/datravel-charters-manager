e866f99b30816beb9a49a941b94527826a5b38a5
Sergey Popinevskiy <gurza000@gmail.com>
2016-08-17 16:36:43 +0300

    Implement 'get source list' function

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 #coding: utf-8
-from flask import Flask
+from flask import Flask, Response
+import simplejson
 import config
 
 
@@ -10,6 +11,18 @@ app.config.from_object(config)
 @app.route('/api/v1/ping', methods = ['GET'])
 def ping():
     return '', 200
+
+
+@app.route('/api/v1/sources', methods = ['GET'])
+def get_sources():
+    source_list = []
+    for source in config.sources:
+        source_list.append({'name': source['name']})
+
+    return Response(
+        simplejson.dumps(source_list, ensure_ascii=False),
+        mimetype='application/json'
+    )
 
 
 if __name__ == '__main__':
